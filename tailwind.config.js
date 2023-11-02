9b4bfbfc0da5d1604b7fa9c0e9bd73be557ccc43
Tristan-Boudra <boudratristan@gmail.com>
2023-11-02 18:13:45 +0100

    feat(app): Initialisation

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#6463F6',
+        'secondary': '#292935',
+        
+      },
+    },
+  },
+  plugins: [],
+}
+
